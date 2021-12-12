8f3b6c8338b57ef15af720f8fc265057fa6dbbab
Tudor <lupashko066@mail.ru>
2021-12-12 20:35:37 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,45 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+// name:"Tudor",
+// age: 30,
+// isMarried:true 
+// };
+// console.log(obj.name);
+
+// let arr = ["plum.png","hust.jpg",10,"green.bpm"]
+// console.log(arr [1]);
+// const result = confirm ("Are you here&");
+// console.log(result);
+// const answer = +prompt('Вам есть 18?', "");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// // answers[0] = prompt("Как вас зовут?");
+// // answers[1] = prompt("Как вас зовут?");
+// // answers[2] = prompt("Как вас зовут?");
+
+// // console.log(typeof(answers));
+// const user = "Tudor";
+
+// alert(`Привет,${user}`);
+// console.log(2*4 === "8");
+const isChecked = true,
+      isClose = true;
+
+console.log(isChecked || isClose);      
+
+
+
+
+
+
+
+
