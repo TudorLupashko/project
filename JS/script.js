176210c54bdf5d7c67c1b45cbef554f714621d6c
Tudor <lupashko066@mail.ru>
2021-12-13 11:55:38 +0300

    result movies

--- JS/script.js
+++ JS/script.js
@@ -26,15 +26,34 @@
 // // answers[1] = prompt("Как вас зовут?");
 // // answers[2] = prompt("Как вас зовут?");
 
-// // console.log(typeof(answers));
-// const user = "Tudor";
+// // // console.log(typeof(answers));
+// // const user = "Tudor";
 
-// alert(`Привет,${user}`);
-// console.log(2*4 === "8");
-const isChecked = true,
-      isClose = true;
+// // alert(`Привет,${user}`);
+// // console.log(2*4 === "8");
+// const isChecked = true,
+//       isClose = true;
 
-console.log(isChecked || isClose);      
+// console.log(isChecked || isClose);      
+const number0fFilms = +prompt ('Сколько фильмов вы уже посмотрели?');
+
+const personalMovieDB = {
+    count: number0fFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?','');
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+
+      console.log(personalMovieDB);
 
 
 
